dd53af0615bb03bea1312a68950511167141766c
Prajakta Sonawane <prajakta.sonawane.in@gmail.com>
2021-09-17 09:16:51 -0700

    add the button_view in views file

--- graph/views.py
+++ graph/views.py
@@ -3,6 +3,9 @@ from .models import Sales
 from .utils import get_plot
 # Create your views here.
 
+def button_view(request):
+    return render(request, "sales/button.html")
+
 def main_view(request):
     qs = Sales.objects.all()
     x = [x.item for x in qs]
