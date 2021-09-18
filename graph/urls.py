e81679727b353e2cc265ea44f64b8cd5d3b05375
Prajakta Sonawane <prajakta.sonawane.in@gmail.com>
2021-09-18 04:21:26 -0700

    add view for histogram and barchart

--- graph/urls.py
+++ graph/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
         path('', views.button_view, name='main_view'),
         path('main_view', views.main_view, name='main_view'),
+        path('hist_view', views.hist_view, name='hist_view'),
+        path('bar_view', views.bar_view, name='bar_view'),
 ]
