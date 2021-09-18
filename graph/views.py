e9ee7ec8d325185750f4f04826d2a537cf9beee6
Prajakta Sonawane <prajakta.sonawane.in@gmail.com>
2021-09-18 04:29:53 -0700

    add view for histogram and barchart

--- graph/views.py
+++ graph/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Sales
-from .utils import get_plot
+import matplotlib.pyplot as plt
+from .utils import get_plot,Histogram,bargraph
 # Create your views here.
 
 def button_view(request):
@@ -12,3 +13,14 @@ def main_view(request):
     y = [y.price for y in qs]
     chart = get_plot(x,y)
     return render(request, 'sales/main.html', {'chart': chart})
+def hist_view(request):
+    qs = Sales.objects.all()
+    x = [x.item for x in qs]  
+    chart = Histogram(x)
+    return render(request, 'sales/main.html',{'chart':chart})
+def bar_view(request):
+    qs = Sales.objects.all()
+    x = [x.item for x in qs]
+    y = [y.price for y  in qs]    
+    chart = bargraph(x,y)
+    return render(request, 'sales/main.html',{'chart':chart})    
